bee4c20b7706deabfa135c6f81c4e3e4d7cb9301
Artem Dorokhov <artdor1978@gmail.com>
2019-09-05 16:05:26 +0300

    still there

--- src/App.js
+++ src/App.js
@@ -115,19 +115,20 @@ class App extends React.Component {
   handleClickNum(e) {
     const updDisplay = this.state.message;
     //updDisplay == 0 ? this.setState({message: e,}): (updDisplay[updDisplay.length-1] === '.' ? (e === "." ? this.setState({message: updDisplay,}):this.setState({message: updDisplay+e,})):this.setState({message: updDisplay+e,}))
-    updDisplay == 0 ? this.setState({message: e,}) : this.setState({message: updDisplay+e,})
+    updDisplay.includes('.')&& e=='.' ? this.setState({message: updDisplay}) : (updDisplay == '0' ? this.setState({message: e,}) : this.setState({message: updDisplay+e,}))
   //https://stackoverflow.com/questions/7959975/how-to-replace-all-but-the-first-occurrence-of-a-pattern-in-string
+  //.match(/(-?\d+\.?\d*)$/)[0]
   }
   handleClickClear(e) {
     this.setState({
-      message: 0
+      message: '0'
     })
   }
   handleClickRes(e) {
     const updDisplay = this.state.message
     this.setState({
       //message: parseFloat(eval(updDisplay.replace(/\.+/g, '.')).toFixed(4))
-      message: parseFloat(eval(updDisplay).toFixed(4)),
+      message: parseFloat(eval(updDisplay).toFixed(4))+'',
     })
   }
   render() {
